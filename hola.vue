463fa101c40115aa8b106b86d7f12293a0ac1f10
David Espino <davidespinog@gmail.com>
2023-09-10 19:29:53 -0600

    .vue files

---
+++ hola.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>Hola desde un archivo .vue</p>
+    <p>{{ text }}</p>
+  </div>  
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        text: 'Hola desde la data de .vue'
+      }
+    }
+  };
+</script>
+
+<style>
+p {
+  color: pink;
+}
+
+</style>
